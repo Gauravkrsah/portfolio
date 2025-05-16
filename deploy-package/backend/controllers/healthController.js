f4c11116a25326fbeb3908defc734b44b6e5d3e6
Gauravkrsah <gauravkumarsah.078@kathford.edu.np>
2025-05-16 11:26:26 +0545

    all done for dep

--- deploy-package/backend/controllers/healthController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getHealthStatus = void 0;
-// Health check controller
-const getHealthStatus = (req, res) => {
-    res.status(200).json({ status: 'OK', message: 'API is running' });
-};
-exports.getHealthStatus = getHealthStatus;
--- deploy-package/backend/routes/healthRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const healthController_1 = require("../controllers/healthController");
-const router = (0, express_1.Router)();
-// Health check route
-router.get('/health', healthController_1.getHealthStatus);
-exports.default = router;
